2c1496c504e13c3446e5a852a62cb0acceb076a2
Denis <denis.rayan@gmail.com>
2017-11-26 17:48:15 +0000

    Criação da estrutura do projeto.

---
+++ src/main/java/tech/yotz/com/StartApplication.java
@@ -0,0 +1,60 @@
+package tech.yotz.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@ComponentScan({"tech.yotz.com"})
+public class StartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartApplication.class, args);
+	}
+	
+	@Bean
+    public MongoTemplate mongoTemplate(final MongoDbFactory mongoDbFactory, final MongoMappingContext context) {
+        MappingMongoConverter converter =
+                new MappingMongoConverter(new DefaultDbRefResolver(mongoDbFactory), context);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory, converter);
+        return mongoTemplate;
+    }
+	
+	@Bean
+    public Docket swaggerSpringMvcPlugin() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .useDefaultResponseMessages(false)
+            .apiInfo(apiInfo())
+            .select()
+            .paths(Predicates.not(PathSelectors.regex("/error.*")))
+            .build();
+    }
+	
+	private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Yotz Start API's")
+            .description("Exposição de serviços")
+            .contact(new Contact("Yotz Start", "https://www.yotz.tech/", "falacomigo@yotz.tech"))
+            .version("1.0.0")
+            .build();
+    }
+}
